62e3dee94de986edc96ff506b82e97a971ded53e
scottydixon <scotty.dixon@yahoo.com>
2023-10-06 09:39:41 -0500

    commit

--- main.js
+++ main.js
@@ -9,10 +9,51 @@ const jobTypes = {
   programmer: 'Any Ship!'
 };
 
-// Your code will go here
 
+class CrewMember {
 
+  constructor(name, job, specialSkill, ship){
 
+    this.name = name;
+    this.job = job;
+    this.specialSkill = specialSkill;
+    this.ship = null;
+  }
+
+  enterShip = (vessel) => {
+    this.ship = vessel
+    vessel.crew.push(this)
+  }
+}
+
+class Ship {
+
+  constructor(name, type, ability){
+    this.name = name;
+    this.type = type;
+    this.ability = ability;
+    this.crew = [];
+  }
+
+  missionStatement = () => {
+    //ability
+
+    if(this.crew.length === 0){
+      return "Can't perform a mission yet."
+    } else {
+      return this.ability;
+    }
+  }
+}
+
+let mav = new Ship('Mars Ascent Vehicle', 'MAV', 'Ascend into low orbit')
+
+console.log(mav);
+
+const crewMember1 = new CrewMember('Rick Martinez', 'pilot', 'chemistry');
+crewMember1.enterShip(mav);
+
+console.log(crewMember1);
 
 
 
